55a3d502ab4fca0a01ad474f6a46a817680808b3
Trej Gun <trejgun@gmail.com>
2016-06-02 14:57:49 +0300

    test: When adding guide to an event, getting mongo duplicate key error bucket-list/common#110

--- test/endpoints/event.js
+++ test/endpoints/event.js
@@ -3,8 +3,10 @@ import assert from "power-assert";
 import moment from "moment-config-trejgun";
 import {startTime, endTime} from "abl-constants/build/date";
 import {mockInChain, cleanUp} from "abl-common/build/test-utils/flow";
+import {getEventInstanceId} from "abl-utils/build/event";
 
 import EventController from "abl-common/build/controllers/operator/event";
+import ApiKeyController from "abl-common/build/controllers/operator/api-key";
 
 import Client from "../../source/index";
 
@@ -14,6 +16,8 @@ const log = debug("test:booking");
 let data;
 
 describe("Event", () => {
+	const eventController = new EventController();
+
 	describe("#getDailyEvents", () => {
 		before(() =>
 			mockInChain([{
@@ -72,8 +76,8 @@ describe("Event", () => {
 		it("should get daily events", () => {
 			const client = new Client(data.ApiKey[0].publicKey, data.ApiKey[0].privateKey);
 			return client.getDailyEvents({
-					date: moment(startTime).add(2, "d").format("YYYY-MM-DD")
-				})
+				date: moment(startTime).add(2, "d").format("YYYY-MM-DD")
+			})
 				.then(obj => {
 					log(obj);
 					assert.equal(obj.list.length, 2);
@@ -81,5 +85,82 @@ describe("Event", () => {
 		});
 
 		after(cleanUp);
+	});
+
+	describe.only("#addGuide", () => {
+		before(() =>
+			mockInChain([{
+				model: "ApiKey",
+				data: [{
+					permissions: ApiKeyController.permissions.dashboard,
+					type: ApiKeyController.types.widget
+				}]
+			}, {
+				model: "Operator",
+				requires: {
+					ApiKey: "o2o"
+				},
+				count: 1
+			}, {
+				model: "Guide",
+				requires: {
+					Operator: "o2o"
+				},
+				count: 1
+			}, {
+				model: "Event",
+				requires: {
+					Operator: "m2o"
+				},
+				data: [{
+					startTime: moment(startTime).add(1, "d"),
+					endTime: moment(endTime).add(1, "d")
+				}]
+			}, {
+				model: "TimeSlot",
+				requires: {
+					Operator: "o2o",
+					Event: "o2m"
+				},
+				count: 1
+			}])
+				.then(result => {
+					data = result;
+				})
+		);
+
+		it("should add guide to event", () => {
+			const eventInstanceId = getEventInstanceId(data.TimeSlot[0].eventId, moment(startTime).add(1, "d"));
+			const client = new Client(data.ApiKey[0].publicKey, data.ApiKey[0].privateKey);
+			return client.addGuideToEvent({
+				eventInstanceId,
+				guide: data.Guide[0]._id
+			})
+				.then(status => {
+					assert.equal(status.success, true);
+					return eventController.findOne({eventInstanceId})
+						.then(event => {
+							assert.equal(event.guides[0].toString(), data.Guide[0]._id.toString());
+						});
+				});
+		});
+
+		it("should add guide to uninitialized event", () => {
+			const eventInstanceId = getEventInstanceId(data.TimeSlot[0].eventId, moment(startTime).add(2, "d"));
+			const client = new Client(data.ApiKey[0].publicKey, data.ApiKey[0].privateKey);
+			return client.addGuideToEvent({
+				eventInstanceId,
+				guide: data.Guide[0]._id
+			})
+				.then(status => {
+					assert.equal(status.success, true);
+					return eventController.findOne({eventInstanceId})
+						.then(event => {
+							assert.equal(event.guides[0].toString(), data.Guide[0]._id.toString());
+						});
+				});
+		});
+
+		//after(cleanUp);
 	});
 });
